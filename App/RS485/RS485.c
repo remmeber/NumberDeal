5916bca8242213ca4ccca0ddecb5bdebd30d1420
rhg_pc <1013773046@qq.com>
2016-10-10 17:56:48 +0800

    change

--- App/RS485/RS485.c
+++ App/RS485/RS485.c
@@ -14,21 +14,20 @@ void UART_485_Configuration(void)
 {
 	
 	USART_InitTypeDef USART_InitStructure;
-	/******************************************************************
-  USART²ÎÊý³õÊ¼»¯:  ²¨ÌØÂÊ     ´«ÊäÎ»Êý   Í£Ö¹Î»Êý  Ð£ÑéÎ»Êý
-                    115200         8         1        0(NO)
+  /******************************************************************
+  USART参数初始化： 波特率     传输位数     停止位数    校验位数 
+                    9600         8         	  1        	 0(NO)
   *******************************************************************/
-  USART_InitStructure.USART_BaudRate = 9600;                       //Éè¶¨´«ÊäËÙÂÊ
-  USART_InitStructure.USART_WordLength = USART_WordLength_8b;        //Éè¶¨´«ÊäÊý¾ÝÎ»Êý
-  USART_InitStructure.USART_StopBits = USART_StopBits_1;             //Éè¶¨Í£Ö¹Î»¸öÊý
-  USART_InitStructure.USART_Parity = USART_Parity_No ;               //²»ÓÃÐ£ÑéÎ»
-  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//²»ÓÃÁ÷Á¿¿ØÖÆ
-  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;    //Ê¹ÓÃ½ÓÊÕºÍ·¢ËÍ¹¦ÄÜ
-  USART_Init(USART1, &USART_InitStructure);                          //³õÊ¼»¯USART1
+  USART_InitStructure.USART_BaudRate = 9600;                     						//设定传输速率
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;       				    //设定传输数据位数
+  USART_InitStructure.USART_StopBits = USART_StopBits_1;            				    //设定停止位个数
+  USART_InitStructure.USART_Parity = USART_Parity_No ;              			 	    //不用校验位
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;		//不用流量控制
+  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;    				    //使用接收和发送功能
+  USART_Init(USART1, &USART_InitStructure);                          				    //初始化USART1
+  USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);                     				    //使能USART1接收中断
 
-  USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);                     //Ê¹ÄÜUSART1½ÓÊÕÖÐ¶Ï
-
-  USART_Cmd(USART1, ENABLE);                                         //Ê¹ÄÜUSART1
+  USART_Cmd(USART1, ENABLE);                                         				     //使能USART1
 	/* Enable driver enable mode */  
   //USART_DECmd(USART1, ENABLE);
 
